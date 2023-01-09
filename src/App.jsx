6a648b0adb8b6cd591d832fcb492662c340f70a3
thepaxx <ironmxke0x@gmail.com>
2023-01-09 16:31:24 +0700

    thepaxx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import *as React from 'react'
+
+
+function App() {
+  const title ='React';
+  
+
+  return (
+    <div>
+      <div>
+        <h1>hello</h1>
+        </div>
+      
+      
+    </div>
+  )
+}
+
+export default App
